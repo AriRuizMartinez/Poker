6c2cb4481a9c1303f24df321a4efad6ef64d180a
AriRuizMartinez <ariruizmartinez@gmail.com>
2024-09-17 13:15:43 +0200

    Clase jugador añadida

---
+++ Poker/Jugador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Jugador
+    {
+        List<Carta> mano;
+
+        public Jugador()
+        {
+            mano = new List<Carta>();
+        }
+    }
+}
